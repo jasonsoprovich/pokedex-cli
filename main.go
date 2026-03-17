24408b5006987f92970ed7d77dfd6746f622b8d6
jasonsoprovich <190052169+jasonsoprovich@users.noreply.github.com>
2026-03-17 15:19:28 -0600

    feat: add basic repl functionality

--- main.go
+++ main.go
@@ -1,8 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello, World!")
-	// scanner.Scan := bufio.NewScanner(os.Stdin)
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,9 +1,30 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	prompt := "Pokedex > "
+
+	for {
+		fmt.Printf("%v", prompt)
+		scanner.Scan()
+		input := scanner.Text()
+		words := cleanInput(input)
+		if len(words) == 0 {
+			continue
+		}
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
 }
