e6426305798c0dc8b12267fd915bfcc004bf72d9
jasonsoprovich <190052169+jasonsoprovich@users.noreply.github.com>
2026-03-17 19:15:00 -0600

    refactor: move commands into separate files

--- repl.go
+++ repl.go
@@ -59,22 +59,3 @@ func getCommands() map[string]cliCommand {
 		},
 	}
 }
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
-	fmt.Println()
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:")
-	fmt.Println()
-
-	for _, cmd := range getCommands() {
-		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
-	}
-	fmt.Println()
-	return nil
-}
